6bc12a4261873d1901b6c67cfeb279cd0ddebbd4
Karol <chmielewski.karol.96@gmail.com>
2016-11-19 12:55:46 +0100

    do poprawy

---
+++ lab1911/petle/zad4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+  int p = 0;
+  int d;
+    printf("Wpisz liczbę większą od 1: ");
+    scanf("%d", &d);
+
+  if (d<1){
+    printf("Błąd, podałeś liczbę mniejszą niż 1");
+  }
+  else {
+    for (p = 0; p <= d; p++)
+printf("cos");
+  }
+  printf("%d\n", p+1);
+
+}
