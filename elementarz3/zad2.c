fc43385858c317511c9eb0c1dee3fef5fdeb6bc5
Karol Chmielewski <chmielewski.karol.96@gmail.com>
2017-01-07 11:35:43 +0100

    .

--- elementarz3/zad2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-int main() {
-  for(int i = 1; i < 21; i++) {
-    printf("Pierwiastek z %d to\t%lf\n", i, sqrt(i));
-  }
-}
--- petle/zad1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int pot=1;
-    while (pot<2010) {
-        printf("%d\n", pot);
-        pot *= 2;
-    }
-    return 0;
-}
