d140776b0f8f0f04ec8b6fd745bb1eac7a853a7a
Karol Chmielewski <chmielewski.karol.96@gmail.com>
2017-01-14 09:54:34 +0100

    zadania

---
+++ wskazniki1/zad5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
+
+int main(void)
+{
+ 	int tab[512];
+ 	int i;
+
+ 	for(i=0; i < ARRAY_SIZE(tab); i++) {tab[i] = 128;}
+
+  for(i = 0; i <ARRAY_SIZE(tab); i++)
+  printf("%d\n", tab[i]);
+}
